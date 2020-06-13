b8cf72338c4802791f3cb97d29785e15cc9ec2d1
Jason <690163223@qq.com>
2020-06-13 14:18:16 +0800

    append

--- src/components/Transition/transition.tsx
+++ src/components/Transition/transition.tsx
@@ -1,25 +1,23 @@
-import React from 'react'
-import { CSSTransition } from 'react-transition-group'
-import { CSSTransitionProps } from 'react-transition-group/CSSTransition'
+import React from "react"
+import { CSSTransition } from "react-transition-group"
+import { CSSTransitionProps } from "react-transition-group/CSSTransition"
 
-type AnimationName = 'zoom-in-top' | 'zoom-in-left' | 'zoom-in-bottom' | 'zoom-in-right'
+type AnimationName =
+  | "zoom-in-top"
+  | "zoom-in-left"
+  | "zoom-in-bottom"
+  | "zoom-in-right"
 
 type TransitionProps = CSSTransitionProps & {
-  animation?: AnimationName,
-  wrapper? : boolean,
+  animation?: AnimationName
+  wrapper?: boolean //解决动画冲突
 }
 
 const Transition: React.FC<TransitionProps> = (props) => {
-  const {
-    children,
-    classNames,
-    animation,
-    wrapper,
-    ...restProps
-  } = props
+  const { children, classNames, animation, wrapper, ...restProps } = props
   return (
     <CSSTransition
-      classNames = { classNames ? classNames : animation}
+      classNames={classNames ? classNames : animation}
       {...restProps}
     >
       {wrapper ? <div>{children}</div> : children}
